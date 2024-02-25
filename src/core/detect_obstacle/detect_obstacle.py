8f15ec1c2d9db0f4e255f0e0181208b03ef63ef5
YangBoZhone <yangbozhone@gmail.com>
2024-02-25 17:21:12 +0800

    新增橫向距離判斷

--- src/core/detect_obstacle/detect_obstacle.py
+++ src/core/detect_obstacle/detect_obstacle.py
@@ -7,7 +7,6 @@ import numpy as np
 from .elevation_view import draw_elevation_view, draw_elevation_view_points
 from .utils import (
     process_missing_points,
-    get_max_obstacle_distance,
     draw_square,
     draw_circle,
     alert,
@@ -15,6 +14,7 @@ from .utils import (
     init_detect_points,
     is_hole,
     is_obstacle,
+    is_safe_area,
 )
 from ..alarm.alarm import Alarm
 from ..realsense_camera.realsense_camera import RealsenseCamera
@@ -26,8 +26,8 @@ class DetectObstacle:
     def __init__(self, config: Any):
         DetectObstacle.instance = self
         self.config_env = config.env["config"]
-        self.od_env = config.env["obstacle_detection"]
-        if self.od_env["missing_point_alarm"]:
+        self.do_env = config.env["obstacle_detection"]
+        if self.do_env["missing_point_alarm"]:
             self.missing_points_buffer = []
         self.alarm = True
         self.detect_points = None
@@ -45,7 +45,7 @@ class DetectObstacle:
         :return: 繪製過後的彩度圖片與深度圖片
         """
         debug = self.config_env["debug"]
-        missing_point_alarm = self.od_env["missing_point_alarm"]
+        missing_point_alarm = self.do_env["missing_point_alarm"]
 
         if debug:
             orig_color_img = color_img.copy()
@@ -53,21 +53,20 @@ class DetectObstacle:
 
         min_hole_distance = math.inf
         min_obstacle_distance = math.inf
-        max_obstacle_dist = get_max_obstacle_distance(self.od_env)
 
         if missing_point_alarm:
             missing_points = []
 
         depth_image = np.asanyarray(depth_frame.get_data())
 
-        area = np.array(self.od_env["area"], np.int32)
+        area = np.array(self.do_env["area"], np.int32)
         color_img = cv2.polylines(color_img, [area], True, (255, 255, 255), 2)
         img_height, img_width = color_img.shape[:2]
 
         if self.detect_points is None:
             spilt_count = 40 if img_height == 480 else 60
             self.detect_points = init_detect_points(
-                self.od_env, img_height, img_width, area, spilt_count
+                self.do_env, img_height, img_width, area, spilt_count
             )
 
         if debug:
@@ -96,7 +95,7 @@ class DetectObstacle:
                     continue
 
                 result = RealsenseCamera.instance.depth_pixel_to_height(
-                    depth_image, depth_pixel, self.od_env["camera_height"]
+                    depth_image, depth_pixel, self.do_env["camera_height"]
                 )
 
                 if not result:
@@ -104,28 +103,23 @@ class DetectObstacle:
                         missing_points.append(index)
                     continue
 
-                height, dist, depth_point = result
+                height, dist, lateral_dist, depth_point = result
                 color = (0, 255, 0)
 
                 if dist > 0:
                     if closest_point is None or dist < closest_point[1]:
                         closest_point = result
                     # 如果高度低於最高坑洞的判斷標準，則代表有坑洞
-                    if is_hole(self.od_env, height):
+                    if is_hole(self.do_env, height, lateral_dist):
                         color = (0, 0, 255)
                         if dist < min_hole_distance:
                             min_hole_distance = dist
                     # 如果高度低於身高，又高於最低障礙物的判斷標準，則代表有障礙物
-                    elif is_obstacle(self.od_env, height, dist):
+                    elif is_obstacle(self.do_env, height, dist, lateral_dist):
                         color = (0, 255, 255)
                         if dist < min_obstacle_distance:
                             min_obstacle_distance = dist
-                    elif (
-                        debug
-                        and self.od_env["lowest_obstacle_height"]
-                        > height
-                        > self.od_env["highest_hole_height"]
-                    ):
+                    elif debug and is_safe_area(self.do_env, height):
                         border_img = cv2.circle(border_img, color_pixel, 50, color, -1)
 
                 if debug:
@@ -140,12 +134,19 @@ class DetectObstacle:
                     color_img = draw_text(
                         color_img,
                         str(int(height)),
-                        (x, y + 10),
+                        (x, y + 15),
                         (255, 255, 255),
                         font_scale,
                     )
                     color_img = draw_text(
-                        color_img, str(dist), (x, y - 10), (255, 200, 255), font_scale
+                        color_img, str(dist), (x, y), (255, 200, 255), font_scale
+                    )
+                    color_img = draw_text(
+                        color_img,
+                        str(lateral_dist),
+                        (x, y - 15),
+                        (255, 0, 0),
+                        font_scale,
                     )
                     depth_img = draw_circle(depth_img, depth_pixel, color)
 
@@ -159,7 +160,7 @@ class DetectObstacle:
         if missing_point_alarm:
             # 緩存消失點，如果所有緩存的消失點都有該點，則警告
             self.missing_points_buffer.append(missing_points)
-            if len(self.missing_points_buffer) > self.od_env["missing_point_threshold"]:
+            if len(self.missing_points_buffer) > self.do_env["missing_point_threshold"]:
                 self.missing_points_buffer = self.missing_points_buffer[1:]
 
             missing_point = process_missing_points(self.missing_points_buffer)
@@ -167,7 +168,7 @@ class DetectObstacle:
             missing_point = None
 
         if Alarm.instance is not None and self.alarm:
-            alert(self.od_env, min_hole_distance, min_obstacle_distance, missing_point)
+            alert(self.do_env, min_hole_distance, min_obstacle_distance, missing_point)
 
         if debug:
             return color_img, depth_img, border_img, elevation_view_img
